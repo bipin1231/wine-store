d3cb9f4085cb6eebd53a86a94a0f77c16da1b201
bipin-adhikari <bipinadhikari332@gmail.com>
2025-09-18 23:19:46 +0545

    handle role

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,39 @@
-import React, { useState ,useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes, RouterProvider, BrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import UserLayout from './components/layout/UserLayout'
-import UserRoutes from './components/routes/UserRoutes'
-import AdminRoutes from './components/routes/AdminRoutes'
-import { setUserInfo,clearUserInfo } from "./redux/userSlice";
-import { useDispatch } from "react-redux";
-import { useGetCurrentUserQuery } from './redux/authApi'
-import { ToastContainer } from "react-toastify";
-
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import UserRoutes from './components/routes/UserRoutes';
+import AdminRoutes from './components/routes/AdminRoutes';
+import { setUserInfo, clearUserInfo } from './redux/userSlice';
+import { useDispatch } from 'react-redux';
+import { useGetCurrentUserQuery } from './redux/authApi';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-
-   const dispatch = useDispatch();
-  const { data, isSuccess,error } = useGetCurrentUserQuery();
-
-
+  const dispatch = useDispatch();
+  const { data, isSuccess } = useGetCurrentUserQuery();
+  const [userType, setUserType] = useState("user");
 
   useEffect(() => {
-if (isSuccess) {
-
-  
-    dispatch(setUserInfo(data.data));
-}else{
-  dispatch(clearUserInfo())
-}
-
-
-  }, [ isSuccess,error,data, dispatch]);
-
-  const userType = "user"
+    if (isSuccess && data?.data) {
+      setUserType(data.data.role);
+      dispatch(setUserInfo(data.data));
+    } else {
+      dispatch(clearUserInfo());
+    }
+  }, [isSuccess, data, dispatch]);
 
   return (
     <BrowserRouter>
-     <ToastContainer position="top-right" autoClose={2000} />
+      <ToastContainer position="top-right" autoClose={2000} />
       <Routes>
-{
-  userType==="admin"?(
-
-      <Route path='/*' element={<AdminRoutes />} />
-  ):(
-      <Route path='/*' element={<UserRoutes />} />
-  )
-}      
-
+        {userType?.toLowerCase() === "admin" ? (
+          <Route path="/*" element={<AdminRoutes />} />
+        ) : (
+          <Route path="/*" element={<UserRoutes />} />
+        )}
       </Routes>
     </BrowserRouter>
-
-
-
-
-  )
+  );
 }
 
-export default App
+export default App;
