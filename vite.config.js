3a466159f0463cbd033b0921d6417ef790085fb4
bipin-adhikari <bipinadhikari332@gmail.com>
2026-05-16 21:43:35 +0545

    feat: add caching

--- vite.config.js
+++ vite.config.js
@@ -47,6 +47,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        maximumFileSizeToCacheInBytes: 5000000,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
